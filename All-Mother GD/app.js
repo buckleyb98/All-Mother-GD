ea0ff63bb1c4ef5a5287fce730ad80aaf8fbce8d
Billyjack Buckley <77069065+buckleyb98@users.noreply.github.com>
2022-05-03 17:59:17 +0100

    Upgraded "Projects", Added "Blog"
    Updated "Projects" page by adding a Modal Lightbox for the images
    Added a "Blog" page
    Added a "Back to Top" button

--- All-Mother GD/app.js
+++ All-Mother GD/app.js
@@ -1,41 +1,6 @@
-// Nav Bar
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
-// Projects
-
-var elements = document.getElementsByClassName("column");
-
-var i;
-
-function one() {
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.flex = "100%";
-    }
-}
-
-function two() {
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.flex = "50%";
-    }
-}
-
-function four() {
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.flex = "25%";
-    }
-}
-
 // Back to Top Button
 
-mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
 window.onscroll = function() {scrollFunction()};
 
@@ -46,11 +11,64 @@ function scrollFunction() {
         } else {
             mybutton.style.display = "none";
         }
-}
+    }
 
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// Nav Bar
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+// Projects Modal
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active","");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+
 
 
